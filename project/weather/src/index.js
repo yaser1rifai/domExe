4d636345ca17f9c68a9370aacfb964eec6218529
[your <rifai.it@gmail.com>
2019-07-16 12:26:13 +0200

    weather Application

---
+++ project/weather/src/index.js
@@ -0,0 +1,33 @@
+
+/* import createBanner from "./banner.js"; */
+import "./style.css";
+import  {myKey} from "./config.js";
+/* import WebpackImg from "./webpack-logo.png"; */
+
+
+/* let myIcon = document.querySelector(".icon"); */
+let myTemperature = document.querySelector(".temperature");
+let myWind = document.querySelector(".wind");
+let myCountry = document.querySelector(".country");
+let myHumidity = document.querySelector(".humidity");
+let myButton = document.querySelector("#btn");
+
+myButton.addEventListener("click", function(){
+    let myCity = document.querySelector("#cityName").value;
+    let api = `http://api.openweathermap.org/data/2.5/weather?q=${myCity}&APPID=${myKey}`;
+
+    fetch(api)
+    .then(response=> {
+        return response.json()
+    })
+    .then(data=>{
+        console.log(data)
+
+      /*  myIcon.innerHTML = data.weather[0].description; */
+        myTemperature.innerHTML += data.main.temp;
+        myWind.innerHTML+= data.wind.speed;
+        myCountry.innerHTML += data.sys.country;
+        myHumidity.innerHTML += data.main.humidity+"%";
+        //myExtra.innerHTML = data. etc.;
+    })
+})
---
+++ project/weather/webpack.config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/,
+          use: ["file-loader"]
+        }
+      ]
+    }
+  };
